b5e5e668bf19057d25f11e3de6c1ab194e0c71a9
ColonelCrockett <163603913+ColonelCrockett@users.noreply.github.com>
2024-03-22 18:11:59 -0400

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,16 @@
+(function(window){
+    // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+    var speakWord = "Good Bye";
+
+    // Create an object, called 'byeSpeaker' to which you will attach the "speak" method and which you will expose to the global context
+    var byeSpeaker = {};
+
+    // Rewrite the 'speak' function such that it is attached to the byeSpeaker object instead of being a standalone function.
+    byeSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    };
+
+    // Expose the 'byeSpeaker' object to the global scope. Name it 'byeSpeaker' on the global scope as well.
+    window.byeSpeaker = byeSpeaker;
+})(window);
+
---
+++ SpeakHello.js
@@ -0,0 +1,15 @@
+(function(window){
+    // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+    var speakWord = "Hello";
+    
+    // Create an object, called 'helloSpeaker' to which you will attach the "speak" method and which you will expose to the global context
+    var helloSpeaker = {};
+
+    // Rewrite the 'speak' function such that it is attached to the helloSpeaker object instead of being a standalone function.
+    helloSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    };
+
+    // Expose the 'helloSpeaker' object to the global scope. Name it 'helloSpeaker' on the global scope as well.
+    window.helloSpeaker = helloSpeaker;
+})(window);
